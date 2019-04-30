4fe82eb5802252d5f55a22598be5ccd887235bb5
Riya-John <47822708+Riya-John@users.noreply.github.com>
2019-04-30 14:05:31 +0530

    Add files via upload

---
+++ six2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int n;
+class Array
+{
+	int a[100],max,min;
+	public:
+	void read()
+	{
+		cout<<"Enter the no. of elements:";
+		cin>>n;
+		cout<<"Enter the elements:";
+		for(int i=0;i<n;i++)
+			cin>>a[i];
+	}
+	void find_Max()
+	{
+		max=a[0];
+		for(int i=0;i<n;i++)
+			if(a[i]>max)
+				max=a[i];
+		cout<<"\n the Maximum is:"<<max;
+	}
+	void find_Min()
+	{
+		min=a[0];
+		for(int i=0;i<n;i++)
+			if(a[i]<min)
+				min=a[i];
+		cout<<"\n the Minimum is:"<<min;
+	}
+};
+int main()
+{
+	int ch,y=1;
+	do
+	{
+		Array *p=new Array;
+		p->read();
+		cout<<"\n1.maximum element\n2.Minimum element\nEnter your choice:";
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:p->find_Max();
+				break;
+			case 2:p->find_Min();
+				break;
+			default:cout<<"Wrong Choice";
+		}
+		cout<<"\nDo you want to continue:";
+		cin>>y;
+	}while(y==1);
+	return 0;
+}
+
+  
+
+
